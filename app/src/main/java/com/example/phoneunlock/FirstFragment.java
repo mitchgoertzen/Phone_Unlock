abfe94e72e958a53abbaf3d73bd762b24efa0135
mitchell.goertzen <mitchell.goertzen@cs.ucalgary.ca>
2021-10-18 21:06:57 -0600

    change app screens

--- app/src/main/java/com/example/phoneunlock/FirstFragment.java
+++ app/src/main/java/com/example/phoneunlock/FirstFragment.java
@@ -26,8 +26,7 @@ public class FirstFragment extends Fragment {
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                System.exit(0);
             }
         });
     }
